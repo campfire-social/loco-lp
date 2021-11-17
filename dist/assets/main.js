bc62a1e239a56065ea15771ac6f53e0bd58a0489
Nathan Skinner <nathan.skinner@bmcjax.com>
2021-11-17 12:08:55 -0500

    added smooths scrolling

---
+++ dist/assets/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll('a[href^="#"]').addEventListener("click",(function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})}))}),!1)})();
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,13 @@
 import '../css/main.css';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.querySelectorAll('a[href^="#"]').addEventListener("click", function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+      });
+
+  }, false);
+
+
