d7f8f319c36fa9fde97768fcce80fec3210288f6
Nathan Skinner <nathan.skinner@bmcjax.com>
2021-11-17 12:01:42 -0500

    edits to nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,11 @@ module.exports = {
               'loco-tan': '#DCCFAF',
               'loco-umber': '#983820'
           },
-          left: {
-            '-50': '-50%'
-          },
-          top: {
+          inset: {
             '18p': '18%',
-            '30p': '30%'
+            '30p': '30%',
+            '-50': '-50%',
+            '-1px': '-1px'
           },
           fontFamily: {
               'sans': [ "Titillium Web", 'Helvetica', 'Arial', 'sans-serif' ],
@@ -22,7 +21,8 @@ module.exports = {
               '-1/2': '-50%',
               '-1/4': '-25%',
               '-1/5': '-20%',
-              '-1/10': '-10%'
+              '-1/10': '-10%',
+              '-1px': '-1px',
           },
           zIndex: {
               '-1': '-1'
