0d833a702f8418b48c7cb6fba41408c36ae6a35a
Nathan Skinner <nathan.skinner@bmcjax.com>
2021-11-17 19:42:16 -0500

    adding aos

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,12 @@
 import '../css/main.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+});
 
 const lazyLoad = (targets, onIntersection) => {
     const observer = new IntersectionObserver((entries, self) => {
